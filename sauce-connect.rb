31c27f58bdf7880ff7e6e1899d72a4b9fb94d322
Raphael Adam <raphael.adam@webfilings.com>
2015-06-17 16:00:44 -0500

    update sha1

--- sauce-connect.rb
+++ sauce-connect.rb
@@ -2,7 +2,7 @@ require "formula"
 class SauceConnect < Formula
   homepage "https://docs.saucelabs.com/reference/sauce-connect/"
   url "https://saucelabs.com/downloads/sc-4.3.9-osx.zip"
-  sha1 "9d9ecb49fbea70186d08aac7de6032546d856379"
+  sha1 "b15857b60c8b6e1105a241c9faebd7896192c1e0"
   def install
     bin.install 'bin/sc'
   end
