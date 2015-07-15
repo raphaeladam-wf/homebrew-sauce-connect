813e62c2584dc7440baebc90ce278e98b6a8ca2c
Mark Rusciano <mark.rusciano@workiva.com>
2015-07-15 15:11:38 -0500

    Update sauce-connect.rb

--- sauce-connect.rb
+++ sauce-connect.rb
@@ -1,8 +1,8 @@
 require "formula"
 class SauceConnect < Formula
   homepage "https://docs.saucelabs.com/reference/sauce-connect/"
-  url "https://saucelabs.com/downloads/sc-4.3.9-osx.zip"
-  sha1 "b15857b60c8b6e1105a241c9faebd7896192c1e0"
+  url "https://saucelabs.com/downloads/sc-4.3.10-osx.zip"
+  sha1 "6ec6f7e2af76a189ed8ceadd31282c5fce1e7dae"
   def install
     bin.install 'bin/sc'
   end
