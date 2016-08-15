ad01b94576aac266a47b665976fc968f5ec86a54
Raphael Adam <raphael.adam@webfilings.com>
2016-08-15 09:31:26 -0500

    Fix sha256

--- sauce-connect.rb
+++ sauce-connect.rb
@@ -2,7 +2,7 @@ require "formula"
 class SauceConnect < Formula
   homepage "https://docs.saucelabs.com/reference/sauce-connect/"
   url "https://saucelabs.com/downloads/sc-4.3.16-osx.zip"
-  sha256: "c41cfb9c71ef77332bac6757708548ce40a557caf99e77355d05890b86141fee"
+  sha256 "c41cfb9c71ef77332bac6757708548ce40a557caf99e77355d05890b86141fee"
   def install
     bin.install 'bin/sc'
   end
