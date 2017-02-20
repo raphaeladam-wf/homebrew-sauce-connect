385cb6022ef046947ee6b84fc3777762a61e86ec
Micah Beeman <micah.beeman@webfilings.com>
2017-02-20 10:49:35 -0600

    Upgrade to Sauce Connect 4.4.3

--- sauce-connect.rb
+++ sauce-connect.rb
@@ -1,8 +1,8 @@
 require "formula"
 class SauceConnect < Formula
   homepage "https://wiki.saucelabs.com/display/DOCS/Sauce+Connect+Proxy"
-  url "https://saucelabs.com/downloads/sc-4.4.2-osx.zip"
-  sha256 "9f7672902c7fbe99283b3a96603b2ebcc2a6383005a65f0d73fd56a3403c760e"
+  url "https://saucelabs.com/downloads/sc-4.4.3-osx.zip"
+  sha256 "1e5cdc19d7ed821eae958bfe939287e6f9a32758e32a648a40b6d80117418b68"
   def install
     bin.install 'bin/sc'
   end
