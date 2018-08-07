5e7929cd11b6d3f5d8c298419e2f47af642bcff5
Raphael Adam <raphael.adam@workiva.com>
2018-08-07 11:10:26 -0500

    Update SC to 4.5.0

--- sauce-connect.rb
+++ sauce-connect.rb
@@ -1,8 +1,8 @@
 require "formula"
 class SauceConnect < Formula
   homepage "https://wiki.saucelabs.com/display/DOCS/Sauce+Connect+Proxy"
-  url "https://saucelabs.com/downloads/sc-4.4.11-osx.zip"
-  sha256 "220d6c29496ce770b6f303557b712ab271161f6c5b4be4e711885986ba87fb80"
+  url "https://saucelabs.com/downloads/sc-4.5.0-osx.zip"
+  sha256 "c97f704102d1878f5a42d08662ed42b250daa0c7c2979f18cbfec0988ad5227f"
   def install
     bin.install 'bin/sc'
   end
