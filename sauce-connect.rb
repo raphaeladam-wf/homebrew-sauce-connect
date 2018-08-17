5fa890cd986f5adb8c2279f7020c3c824e3441e0
Raphael Adam <raphael.adam@webfilings.com>
2018-08-17 09:16:09 -0500

    Update sha256 for SC
    Download has changed

--- sauce-connect.rb
+++ sauce-connect.rb
@@ -2,7 +2,7 @@ require "formula"
 class SauceConnect < Formula
   homepage "https://wiki.saucelabs.com/display/DOCS/Sauce+Connect+Proxy"
   url "https://saucelabs.com/downloads/sc-4.5.0-osx.zip"
-  sha256 "c97f704102d1878f5a42d08662ed42b250daa0c7c2979f18cbfec0988ad5227f"
+  sha256 "6346b99c467a77eb433fc0e7afe3de53a4c8da0aed063e97d766cf15ced468ea"
   def install
     bin.install 'bin/sc'
   end
